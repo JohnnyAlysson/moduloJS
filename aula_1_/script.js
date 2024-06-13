81dd144b9d7c89f0ae2d92bc6a1acf28d68c28e6
JohnnyAysson <johnalysson30@gmail.com>
2024-06-13 12:23:10 -0300

    first JS test

---
+++ aula_1_/script.js
@@ -0,0 +1,31 @@
+// var nome = "Johnny";
+// let age , isHuman;
+
+// age = 26;
+isHuman = true;
+
+// console.log(`Ola, ${nome} voce tem ${age}  anos humano?(${isHuman})`);
+
+const person ={
+  name : "Johnny",
+  age : 28,
+  weight : 72,
+  isAdmin : false
+};
+
+// console.log(` ${person.name} tem ${person.age} anos e pesa ${person.weight} e nao e admin(${person.isAdmin})` )
+// console.log(`${person}`)
+
+const animals = [
+  "lion",
+  "tiger",
+  "monkey",
+  {
+    nome:"cat",
+    age : 3
+  },
+  isHuman,
+  person
+]
+
+console.log(animals[5])
