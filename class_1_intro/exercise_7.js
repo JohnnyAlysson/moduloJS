182415e01b4d1fc0bf0b6200e9a0b12d1032794d
JohnnyAysson <johnalysson30@gmail.com>
2024-06-14 08:22:56 -0300

    Exercise 7 solved

---
+++ class_1_intro/exercise_7.js
@@ -0,0 +1,16 @@
+/* 
+Atividade 07
+Solicite ao usuário que insira um valor em metros usando o prompt e, em
+seguida, converta esse valor para centímetros (1 metro = 100 centímetros) e
+exiba o resultado no console.
+*/
+
+var meters
+var centimeters
+
+meters= prompt("What's the value you would like to convert to Centimeter:")
+
+centimeters = meters * 100
+
+console.log(` Final result is ${centimeters} CM`)
+alert(` Final result is ${centimeters} CM`)
