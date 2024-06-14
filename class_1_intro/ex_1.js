09b2fd80bbfa7c800cce39240d92831129070b76
JohnnyAysson <johnalysson30@gmail.com>
2024-06-13 21:08:51 -0300

    solved exercise 1

---
+++ class_1_intro/ex_1.js
@@ -0,0 +1,15 @@
+/* 
+Atividade 01
+Crie um programa que peça ao usuário para digitar seu nome
+usando o prompt e, em seguida, exiba o nome com uma frase
+personalizada com template string na tela usando o console.
+*/
+var nameUser
+
+nameUser = prompt("Digite seu nome:")
+
+console.log(` Hello ${nameUser} how are you today? `)
+
+
+
+alert(` Hello ${nameUser} I hope you are good! `)
