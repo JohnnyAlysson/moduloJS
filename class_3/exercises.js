ac62c89fc6ab866c4ddc6d9e8079846a4a164d85
JohnnyAysson <johnalysson30@gmail.com>
2024-06-28 01:15:00 -0300

    Solve exercises and start challenge 3

--- class_3/exercises.js
+++ class_3/exercises.js
@@ -1,7 +1,7 @@
-// Atividade 01
-// Crie um programa que solicita ao usuário um número e, em seguida,
-// conta regressivamente até zero, imprimindo cada número no console.
-// O programa deve terminar quando atingir zero.
+// // Atividade 01
+// // Crie um programa que solicita ao usuário um número e, em seguida,
+// // conta regressivamente até zero, imprimindo cada número no console.
+// // O programa deve terminar quando atingir zero.
 // var contador = Number(prompt("Digite o numero desejado:"))
 
 // while(contador >= 0){
@@ -45,27 +45,112 @@
 // alert(`Sua media e ${media}`)
 
 
-// Atividade 03
-// Peça ao usuário para inserir números continuamente e imprima o
-// somatório dos números inseridos. O programa deve continuar executando
-// até que o usuário insira zero.
+// // Atividade 03
+// // Peça ao usuário para inserir números continuamente e imprima o
+// // somatório dos números inseridos. O programa deve continuar executando
+// // até que o usuário insira zero.
 
-// Atividade 04
-// Faça um programa que leia um nome de usuário e a sua senha e não
-// aceite a senha igual ao nome do usuário, mostrando uma mensagem de
-// erro e voltando a pedir as informações.
-// Atividade 05
-// Faça um programa que peça para 5 pessoas a sua idade, ao final o
-// programa devera verificar se a média de idade da turma varia entre 0 e 25,
-// 26 e 60 e maior que 60; e então, dizer se a turma é jovem, adulta ou idosa,
-// conforme a média calculada.
+// var somatorio = 0
+// var contador = 0
 
-// Atividade 06
-// Calcule a soma dos números de 1 a 50
-// Atividade 06
-// Crie um programa que solicite um número de 1 a 7, representando
-// o dia da semana. Use uma estrutura switch (ou equivalente) para
-// imprimir o nome do dia correspondente.
-// Atividade 07
-// Escreva um programa que solicite um número e determine se ele é
-// um número primo.
+
+// console.log("Programa para calculo de somatorio. \n Digite '0' para terminar o programa")
+// alert("Programa para calculo de somatorio. \n Digite '0' para terminar o programa")
+// while(true){
+//   var  user = prompt(`Digite o numero${contador + 1}`)
+//   if(user == "0"){
+//     break
+//   }
+//   else if(!isNaN(user)== false){
+//     console.log("tente novamente, input invalido")
+//   }
+//   else{
+//     somatorio = somatorio + Number(user)
+//     contador ++
+//     console.log(`${somatorio},${contador}`)
+//   }
+// }
+// console.log(`O somatorio dos numeros e ${somatorio}`)
+// alert(`O somatorio dos numeros e ${somatorio}`)
+
+// // Atividade 04
+// // Faça um programa que leia um nome de usuário e a sua senha e não
+// // aceite a senha igual ao nome do usuário, mostrando uma mensagem de
+// // erro e voltando a pedir as informações.
+
+// var usuario
+// var senha
+// while(true){
+//   usuario = prompt("Digite seu usuario")
+//   senha = prompt("Digite sua senha")
+
+//   if (usuario == senha){
+//     alert("usuario e senhas nao podem ser iguais, tente novamente")
+//     console.log("usuario e senhas nao podem ser iguais, tente novamente")
+//   }
+//   else{
+//     break
+//   }
+// }
+
+// alert(`${usuario} Seja bem vindo !`)
+// console.log(`${usuario} Seja bem vindo !`)
+
+
+// // Atividade 05
+// // Faça um programa que peça para 5 pessoas a sua idade, ao final o
+// // programa devera verificar se a média de idade da turma varia entre 0 e 25,
+// // 26 e 60 e maior que 60; e então, dizer se a turma é jovem, adulta ou idosa,
+// // conforme a média calculada.
+
+// var somatorio = 0
+// var contador = 0
+// var media
+
+
+// console.log("media da idade de uma turma")
+// alert("media da idade de uma turma")
+
+// var qtd_alunos = Number(prompt("Quantos alunos essa turma possui?")) //5
+
+// while(contador < qtd_alunos){
+//   var  user = prompt(`Digite o idade da pessoa ${contador + 1}`)
+//   if(!isNaN(user)== false){
+//     console.log("tente novamente, input invalido")
+//   }
+//   else{
+//     somatorio = somatorio + Number(user)
+//     contador ++
+//     console.log(`${somatorio},${contador}`)
+//   }
+// }
+
+// media = somatorio/qtd_alunos
+
+// if (media > 60){
+//   console.log(`O somatorio das idades e ${somatorio} sendo a media ${media} \n Essa e uma turma velha`)
+// }
+// else if(media <= 60 && media > 25){
+//   console.log(`O somatorio das idades e ${somatorio} sendo a media ${media} \n Essa e uma turma Adulta`)
+  
+// }
+// else if (media <=25 && media >0){
+//   console.log(`O somatorio das idades e ${somatorio} sendo a media ${media} \n Essa e uma turma jovem`)
+// }
+// else{
+//   console.log(`algo deu errado tente novamente\n estes foram os valores finais: idades = ${somatorio} ,media= ${media}`)
+// }
+
+
+// // Atividade 06
+// // Calcule a soma dos números de 1 a 50
+
+// var soma = 0
+// var contador =0
+
+// while(contador <= 50){
+//   soma = soma+ contador
+//   contador++
+// }
+
+// console.log(` A soma dos numeros de 1 a 50 e ${soma} `)
---
+++ js/challenge3.js
@@ -0,0 +1,21 @@
+// DESAFIO PRÁTICO  Você foi desafiado a criar um simulador de caixa eletrônico em JavaScript. 
+// O programa deve permitir que o usuário interaja com sua conta bancária simulada, 
+// realizando operações como verificar o saldo, fazer saques, fazer  depósitos e sair do programa.  
+// Aqui estão os requisitos do programa: 
+
+// O programa deve começar com um saldo inicial de $1000.  
+// Utilize um loop while para manter o programa em execução  até que o usuário escolha sair. 
+
+// A cada iteração do loop, o programa deve exibir um menu com as seguintes opções:  
+// Ver Saldo Fazer Saque Fazer Depósito  Sair  Se o usuário escolher  "Ver Saldo " , o programa  deve exibir o saldo atual.  
+
+// Se o usuário escolher  "Fazer Saque " , o programa deve solicitar o valor do saque. 
+// Certifique-se de validar se o valor é numérico,  maior que zero e não excede o saldo  disponível. 
+// Se a validação for bem-sucedida, atualize o saldo e exiba uma mensagem  indicando o sucesso da transação.  
+
+// Se o usuário escolher  "Fazer Depósito " , o programa deve solicitar o valor do depósito. 
+// Certifique-se de validar se o valor é numérico e maior que zero. Se a validação for bem sucedida,  
+// atualize o saldo e exiba uma mensagem indicando o sucesso da transação.  
+
+// Se o usuário escolher  "Sair " , encerre o programa  exibindo uma mensagem de despedida. 
+// Se o usuário escolher uma opção inválida, exiba uma mensagem indicando isso e permita que o  usuário faça uma nova escolha.
