cca0feb141d4e5eb3ba8042276fe582c165d9f72
JohnnyAlysson <149841185+JohnnyAlysson@users.noreply.github.com>
2024-08-14 22:58:47 -0300

    start class 10- project

---
+++ class_10-Project/exercises.js
@@ -0,0 +1,23 @@
+// ATIVIDADE PRÁTICA
+
+// Atividade 01
+// Crie um formulário com campos de nome, email e senha. Adicione um
+// evento de submit ao formulário que valida se todos os campos foram
+// preenchidos. Se não, exiba uma mensagem de erro.
+
+
+
+// Atividade 02
+// Crie um formulário com um campo de texto e um botão. Ao clicar no
+// botão, adicione um novo elemento à página com o texto digitado no
+// campo de texto.
+
+
+
+// Atividade 03
+// Crie uma função que recebe um array de números e retorna a soma deles.
+
+// Atividade 04
+// Crie uma função que calcula o Índice de Massa Corporal (IMC) e
+// retorna se a pessoa está abaixo do peso, no peso ideal ou acima do
+// peso.
