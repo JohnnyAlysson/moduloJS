6e1dc7749ec7276219101f39a05edd4f5c354799
JohnnyAysson <johnalysson30@gmail.com>
2024-08-24 14:44:42 -0300

    start project class 10

--- class_10-Project/exercises.js
+++ class_10-Project/exercises.js
@@ -6,6 +6,48 @@
 // preenchidos. Se não, exiba uma mensagem de erro.
 
 
+function autenticar(username, password, email, onSuccess, onFailure){
+  if (username === "usuario" && password === "senha123" && email === "hadouken@email.com") {
+    onSuccess(username.toLowerCase());
+    cleanForm();
+    } 
+  else {
+    onFailure();
+    cleanForm();
+}
+}
+
+const loginSuccess = (username) => {
+  const messageElement = document.getElementById('message');
+  messageElement.textContent = `Login bem-sucedido! Bem-vindo, ${username}.`;
+  messageElement.style.color = 'green';
+}
+
+const loginFailure = () => {
+  const messageElement = document.getElementById('message');
+  messageElement.textContent = 'Falha na autenticação. Por favor, verifique suas credenciais.';
+  messageElement.style.color = 'red';
+};
+
+const submitForm = () => {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  const email = document.getElementById('email').value;
+
+  autenticar(username, password, email, loginSuccess, loginFailure);
+}
+
+const cleanForm = () =>{
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+  let email = document.getElementById('email').value;
+
+  username = "";
+  password = "";
+  email = "";
+}
+
+
 
 // Atividade 02
 // Crie um formulário com um campo de texto e um botão. Ao clicar no
